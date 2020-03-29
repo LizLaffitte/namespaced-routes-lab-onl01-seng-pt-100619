09a778d4caf6891c1f8aee95040cb7b5fdc7ef31
LizLaffitte <liz.kossakowski@gmail.com>
2020-03-29 14:22:28 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,11 +4,16 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    
     @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    if Preference.first.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
